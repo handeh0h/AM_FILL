2f59f0ef2ef4cdc635fefd444df9bb75b58f4c26
miktim <miktim@mail.ru>
2017-12-24 09:23:28 +0300

    Create Расчетный_лист.sql

---
+++ Demo/Расчетный_лист.sql
@@ -0,0 +1,40 @@
+
+Procedure am_fill_example
+( p_xlsx in out nocopy BLOB  -- шаблон листка
+)
+as
+  type tp_names is table of varchar2(100);
+  type tp_sums is table of number(10,2);
+-- Для вывода переменной части листка необходима таблица (матрица)
+-- значений, которая для простоты представлена набором векторов.
+-- Расчетная часть опущена.
+  l_inames tp_names := tp_names('Оклад','Премия', 'Отпуск','всего'); 
+  l_isums  tp_sums := tp_sums(60000, 120000, 40000, 220000); 
+  l_idays  tp_names := tp_names('32 дн','I кв','20 дн');
+  l_rnames tp_names := tp_names('НДФЛ','','','всего удержано');
+  l_rsums  tp_sums := tp_sums(28600, null, null, 28600); 
+begin
+-- инициализация шаблоном с разрешением exception по ошибке имени
+  am_fill.init(p_xlsx,'e');
+-- заполнение именованных областей титульной части 
+  am_fill.in_field(sysdate, 'Расчетная_дата');
+  am_fill.in_field('Привалов Александр Иванович', 'ФИО_сотрудника');
+  am_fill.in_field('заведующий', 'Должность'); 
+  am_fill.in_field('вычислительный центр', 'Подразделение'); 
+  am_fill.in_field(l_isums(1), 'Оклад'); 
+-- заполнение переменной части в режиме последовательной вставки строк 
+  for k in 1..3 loop
+-- колонки (поля) предпочтительно именовать
+    am_fill.in_field(l_inames(i), 'Нач_Вид', 'i'); 
+    am_fill.in_field(l_isums(i),  'C8', 'i'); 
+    am_fill.in_field(l_idays(i),  'D8', 'i'); 
+    am_fill.in_field(l_rnames(i), 'E8', 'i');
+    am_fill.in_field(l_rsums(i),  'G8', 'i');
+  end loop;
+-- заполнение итоговой части
+  am_fill.in_field(l_isums(4), 'C9');
+  am_fill.in_field(l_rsums(4), 'G9');
+  am_fill.in_field(l_isums(4)-l_rsums(4), 'К_выплате');
+-- формирование документа
+  am_fill.finish(p_xlsx);
+end;
